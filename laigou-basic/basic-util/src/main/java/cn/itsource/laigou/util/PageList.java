cf3a5f530e627213084abb6fdd2b4d3f137fb043
zhangsan <zhangsan@itsource.cn>
2019-10-11 19:27:08 +0800

    项目经理提交项目构架代码

---
+++ laigou-basic/basic-util/src/main/java/cn/itsource/laigou/util/PageList.java
@@ -0,0 +1,38 @@
+package cn.itsource.laigou.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 分页工具类
+ * @author solargen
+ */
+public class PageList<T> {
+
+    private Integer total = 0;
+    private List<T> rows = new ArrayList<>();
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    public List<T> getRows() {
+        return rows;
+    }
+
+    public void setRows(List<T> rows) {
+        this.rows = rows;
+    }
+
+    public PageList() {
+    }
+
+    public PageList(Integer total, List<T> rows) {
+        this.total = total;
+        this.rows = rows;
+    }
+}
